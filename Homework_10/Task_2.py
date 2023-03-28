ee59861ca39075f6af29aa7761349ff790351580
Natalia <natalia.evichenko@mail.ru>
2023-03-28 14:16:27 +0300

    Added task 2 for hm10

---
+++ Homework_10/Task_2.py
@@ -0,0 +1,19 @@
+"""
+Задание 2.
+
+Каждое из слов «class», «function», «method» записать в байтовом формате
+без преобразования в последовательность кодов
+не используя!!! методы encode и decode)
+и определить тип, содержимое и длину соответствующих переменных.
+
+Подсказки:
+--- b'class' - используйте маркировку b''
+--- используйте списки и циклы, не дублируйте функции
+"""
+
+words = ['class', 'function', 'method']
+words_b = []
+for el in words:
+    words_b.append(bytes(el, 'utf-8'))
+for el in words_b:
+    print(f"word: {el}, type: {type(el)}, length: {len(el)}")
